ddedd369fcb15f02af3efc928e1e273638cd9109
NicholasHorta <nickdeveloper.js@gmil.com>
2021-10-04 18:52:54 +0100

    Working on the navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { TweetsComponent } from './components/tweets/tweets.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './components/about/about.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TweetsComponent,
+    HeroComponent,
+    FooterComponent,
+    AboutComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
